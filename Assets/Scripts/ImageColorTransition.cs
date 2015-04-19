082d03e4e8220cfb5b7a49126a6b94c769b9efb1
fbrizu <frank.brizuela@mail.mcgill.ca>
2015-04-19 15:28:16 -0400

    Added funky colors

---
+++ Assets/Scripts/ImageColorTransition.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageColorTransition : MonoBehaviour {
+	public Color[] colors;
+	public float transitionTime;
+	public bool colorsChanging;
+
+	private Image img;
+	private int currentColor, nextColor;
+	private float timer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		img = GetComponent<Image>();
+
+		if(colors.Length > 0) 
+		{			
+			currentColor = 0;
+
+			if(colors.Length > 1)
+				nextColor = 1;
+		}
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+
+		if(colorsChanging)
+		{
+			if(colors.Length > 1)
+			{
+				img.color = Color.Lerp(colors[currentColor], colors[nextColor], Mathf.Clamp(timer / transitionTime, 0, 1));
+			}
+
+			if(timer > transitionTime)
+			{
+				timer = timer % transitionTime;
+				currentColor = nextColor;
+				nextColor = (nextColor + 1 < colors.Length) ? nextColor + 1 : 0;
+			}
+		}
+	}
+}
--- Assets/Scripts/PowerBar.cs
+++ Assets/Scripts/PowerBar.cs
@@ -8,16 +8,10 @@ public class PowerBar : MonoBehaviour {
 	public int _framesPerColour;
 	Image _bar;
 	float _currentPower;
-	int _colourID;
-	Color[] _colours;
-	int _currentFrameCount;
 
 
 	void Start () {
 		_bar = GetComponent<Image> ();
-		_colourID = 0;
-		_colours = new Color[] { Color.cyan, Color.red, Color.green, Color.magenta, Color.yellow, Color.blue} ;
-		_currentFrameCount = _framesPerColour;
 	}
 	void Update () {
 		//Update bar
@@ -27,12 +21,7 @@ public class PowerBar : MonoBehaviour {
 		}
 		//Make bar all flashy if it's full
 		if (_bar.fillAmount == 1) {
-			_currentFrameCount = (_currentFrameCount - 1) % _framesPerColour;
-			if (_currentFrameCount == 0) {
-				_currentFrameCount = _framesPerColour;
-				_bar.color=_colours[_colourID];
-				_colourID = (_colourID+1) % _colours.Length;
-			}
+			GetComponent<ImageColorTransition>().colorsChanging = true;
 		}
 	}
 }

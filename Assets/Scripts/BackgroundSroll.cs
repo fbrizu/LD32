c04b0a2bbee471b2ad43dbc022a55b3439d33f97
Monica Ung <monicaung3@gmail.com>
2015-04-19 20:31:16 -0400

    Added moving background in combo bars!

---
+++ Assets/Scripts/BackgroundSroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSroll : MonoBehaviour {
+
+	public int speed;
+	public int _range;
+	RectTransform _image;
+
+	void Start() {
+		_image = GetComponent<RectTransform> ();
+	}
+	void Update () {
+		_image.anchoredPosition = new Vector3((_image.anchoredPosition.x + speed)% _range, 0, 0);
+	}
+}
---
+++ Assets/Scripts/ComboBarScrollView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ComboBarScrollView : MonoBehaviour {
+
+	public Hoop _playerHoop;
+	public Text _text;
+	RectTransform _bar;
+	float _currentPower;
+	
+	
+	void Start () {
+		_bar = GetComponent<RectTransform> ();
+	}
+	void Update () {
+		//Update bar
+		_currentPower = (float) _playerHoop._comboCount / _playerHoop._maxCombo;
+		_bar.sizeDelta = new Vector2 (_currentPower*150.0f, 30.0f);
+
+		if ( _currentPower == 1) {
+			_text.text = _text.text.Replace("POWER", "OVERDRIVE!");
+		}
+	}
+}

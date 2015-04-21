fb675b6d8ae24c3e2e51caf330110da0014e7b00
Monica Ung <monicaung3@gmail.com>
2015-04-20 21:35:17 -0400

    Added tutorial and prettified win panel a bit

---
+++ Assets/Scripts/ClosePanel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClosePanel : MonoBehaviour {
+
+	public void DisableObject() {
+		gameObject.SetActive(false);
+	}
+
+}

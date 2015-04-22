401c65c82b2d3c256825d43acf1e7bbea5bcb4f1
fbrizu <frank.brizuela@mail.mcgill.ca>
2015-04-22 00:55:23 -0400

    fixed Jarvis typo, reduced hp to 30

--- Assets/Scripts/TimingBar.cs
+++ Assets/Scripts/TimingBar.cs
@@ -5,14 +5,16 @@ using System.Collections;
 public class TimingBar : MonoBehaviour {
 	float _currentX;
 	Image _knob;
+	float initialTime;
 
 	void Start () {
 		transform.localPosition = new Vector3(0,0,0);
 		_knob = transform.GetComponent<Image> ();
 		_knob.color = Color.white;
+		initialTime = Time.time;
 	}
 	void Update () {
-		_currentX = Mathf.PingPong (Time.time, 1) * 100;
+		_currentX = Mathf.PingPong (Time.time - initialTime, 1) * 100;
 		transform.localPosition = new Vector3 (_currentX, 5, 0);
 		if (HoopSpinner._timeToTwist) {
 			_knob.color = Color.red;
